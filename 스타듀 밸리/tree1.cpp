debf50307b8be1604dc271e0931b7acd7d1737b2
Jericho <dldydwp76@gmail.com>
2017-10-14 17:48:07 +0900

    동진이에게 ♡

---
+++ 스타듀 밸리/tree1.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "tree1.h"
+
+
+//tree1::tree1()
+//{
+//}
+//
+//
+//tree1::~tree1()
+//{
+//}
+//
+//
+//
+//HRESULT tree1::init(tagFloat pos)
+//{
+//	motherObject::init("tree1", "tileSprite", pos, pivot::LEFT_TOP);
+//
+//	_hp = 5;
+//	//tree1_top;
+//	_pObj = new tree1_top;
+//
+//	tree1_top->init(pos);
+//	TOWNWORLD->addObject(objectType::OBJ, tree1_top);
+//	TOWNWORLD->addObject(objectType::OBJ, this);
+//
+//	return S_OK;
+//}
+//void tree1::release()
+//{
+//
+//}
+//void tree1::update()
+//{
+//
+//}
+//void tree1::render()
+//{
+//
+//}
