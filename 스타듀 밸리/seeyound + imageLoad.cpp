b11690cbdfcbcd2deeb800b603e10d7c15f7e423
박시영 <soso667@naver.com>
2017-10-15 21:05:39 +0900

    플레이어 이미지 수정 및 나중에 클릭에 따른 상태 변경 미완성각이죠 오지죠?

--- 스타듀 밸리/seeyound + imageLoad.cpp
+++ 스타듀 밸리/seeyound + imageLoad.cpp
@@ -5,7 +5,7 @@ void loadingScene::seeyoung()
 {
 
 	//초기화
-	_loading->loadFrameImage("player", "resource/player/플레이어.bmp", 3000, 4500, 12, 15, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("player", "resource/player/플레이어.bmp", 3000, 4500, 12, 18, true, RGB(255, 0, 255));
 	
 
 
