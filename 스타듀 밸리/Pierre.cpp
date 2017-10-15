d329a129407f1d521e1dfb6dd7699dfc1780cd8b
장영재 <aquaox7@naver.com>
2017-10-15 17:08:22 +0900

    NPC 수정, NPC Pierre 추가

---
+++ 스타듀 밸리/Pierre.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "Pierre.h"
+
+HRESULT Pierre::init()
+{
+
+	return S_OK;
+}
+
+void Pierre::release()  
+{
+
+}
+
+void Pierre::update()   
+{
+
+}
+
+void Pierre::render()   
+{
+
+}
