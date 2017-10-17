1952b8d7d489b6f5d6332971b7e11b458e996ce5
Jericho <dldydwp76@gmail.com>
2017-10-17 22:48:54 +0900

    이미지 로드 제대로 함

--- 스타듀 밸리/jericho + imageLoad.cpp
+++ 스타듀 밸리/jericho + imageLoad.cpp
@@ -4,11 +4,11 @@
 void loadingScene::jericho()
 {
 	//타일 크기 16
-	IMAGEMANAGER->addFrameImage("crops", "Crops(256,672,16,42).bmp", 256, 672, 16, 42, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addImage("farmBuildings", "Farm Buildings(913,412).bmp", 913, 412, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("farmHouse", "Farmhouse(192,320,12,20).bmp", 192, 320, 12, 20, true, RGB(255, 0, 255));
-	//IMAGEMANAGER->addFrameImage("outdoorsSpring", "Outdoors Spring(400,1264,25,79) - 그림자 제거.bmp", 400, 1264, 25, 79, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("tileSprite", "Outdoors Spring(400,1264,25,79).bmp", 400, 1264, 25, 79, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addImage("trees", "Trees(176,645).bmp", 176, 645, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("tileSample", "tileSample(960,960,60,60).bmp", 960, 960, 60, 60, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("crops", "Crops(256,672,16,42).bmp", 256, 672, 16, 42, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("farmBuildings", "Farm Buildings(913,412).bmp", 913, 412, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("farmHouse", "Farmhouse(192,320,12,20).bmp", 192, 320, 12, 20, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("outdoorsSpring", "Outdoors Spring(400,1264,25,79).bmp", 400, 1264, 25, 79, true, RGB(255, 0, 255));
+	//_loading->loadFrameImage("tileSprite", "Outdoors Spring(400,1264,25,79).bmp", 400, 1264, 25, 79, true, RGB(255, 0, 255));
+	_loading->loadImage("trees", "Trees(176,645).bmp", 176, 645, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("tileSample", "tileSample(960,960,60,60).bmp", 960, 960, 60, 60, true, RGB(255, 0, 255));
 }
