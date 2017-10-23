1cccb0e85657855d6c68efca964f44ea858faf85
GodDongjin <wad0125@naver.com>
2017-10-23 17:50:38 +0900

    씨 패치

--- 스타듀 밸리/seed2.cpp
+++ 스타듀 밸리/seed2.cpp
@@ -9,8 +9,16 @@ HRESULT seed2::init(string name, string imageKey, tagFloat pos, string explain)
 {
 	motherObject::init(name, imageKey, pos, pivot::LEFT_TOP);
 
-	_objEnum = OBJECT::SEED2;
+	_objEnum = OBJECT::SEED;
 	TOWNWORLD->getTile(_pos.x / TILESIZE, _pos.y / TILESIZE)->getPObj()->setPObj(this);
+
+	int indexX = _pos.x / TILESIZE;
+	int indexY = _pos.y / TILESIZE;
+
+	mapToolTile* tile = TOWNWORLD->getTile(indexX, indexY);
+
+	_rc = RectMake(tile->getRect().left, tile->getRect().top, _image->getWidth(), _image->getHeight());
+
 
 	//EFFECTMANAGER->addEffect("seed", "¾¾¸ÂÀ»¶§.bmp", 832, 62, 32, 62, 1.0f, 1.0f, 1000);
 	this->addCallback("pickAttack", [this](tagMessage msg)
@@ -35,7 +43,7 @@ HRESULT seed2::init(string name, string imageKey, tagFloat pos, string explain)
 }
 void seed2::release()
 {
-	TOWNWORLD->getTile(_pos.x / TILESIZE, _pos.y / TILESIZE)->getPObj()->setPObj(this);
+	TOWNWORLD->getTile(_pos.x / TILESIZE, _pos.y / TILESIZE)->getPObj()->setPObj(NULL);
 
 	motherObject::release();
 }
@@ -48,32 +56,38 @@ void seed2::update()
 void seed2::grow()
 {
 	if (_growNum >= _image->getMaxFrameX())return;
-	//if (((land*)TOWNWORLD->getTile(_pos.x / TILESIZE, _pos.y / TILESIZE)->getPObj())->getWet() == true)
-	//{
-	_growNum++;
-	if (_growNum == _image->getMaxFrameX())
+	if (((land*)TOWNWORLD->getTile(_pos.x / TILESIZE, _pos.y / TILESIZE)->getPObj())->getWet() == true)
+	{
+		_growNum++;
+
+		if ((land*)TOWNWORLD->getTile(_pos.x / TILESIZE, _pos.y / TILESIZE)->getPObj() == NULL)return;
+
+		((land*)TOWNWORLD->getTile(_pos.x / TILESIZE, _pos.y / TILESIZE)->getPObj())->setWet(false);
+
+
+	}
+	if (_growNum >= _image->getMaxFrameX())
 	{
 		_isRight = true;
 	}
-	//}
+
 
 }
 void seed2::growAttack()
 {
-	EFFECTMANAGER->play("seed", _pos.x, _pos.y);
+	
 
 	this->setDestroy();
 
-	// dropItem* drop = new dropItem;
-	// drop->init("seed", "seed");
+
 }
 void seed2::render()
 {
 	motherObject::render();
 	RECT rc = CAMERAMANAGER->getRenderRc();
 
-	_image->alphaScaleFrameRender(getMemDC(), -rc.left + _pos.x, -rc.top + _pos.y - 10,
-		_growNum, 0, _image->getFrameWidth()*4.0f, _image->getFrameHeight()*4.0f, 255);
+	_image->alphaScaleFrameRender(getMemDC(), -rc.left + _rc.left, -rc.top + _rc.top - 10,
+		_growNum, 0, 65, 65, 255);
 
 	// RectangleMake(getMemDC(), 100-rc.left, 100-rc.top, 100, 100);
 }
